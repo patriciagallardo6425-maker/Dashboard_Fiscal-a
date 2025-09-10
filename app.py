bbe229c75f353670ef19f4b450db1be73be5b8d9
patriciagallardo6425-maker <patricia.gallardo6425@gmail.com>
2025-09-10 08:59:46 -0500

    Hola

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ df = pd.read_csv(url, sep=";", encoding="utf-8")
 
 st.dataframe(df)
 #Elegir columnas
-seleccion_columnas = ["FECHA_HECHOS", "DELITO", "ETAPA", "FISCAL_ASIGNADO", "DEPARTAMENTO"]
-#df= df[seleccion_columnas].sort_values(by="FECHA_HECHOS", ascending=True).reset_index(drop=True)
+seleccion_columnas = ["FECHA_HECHOS", "DELITO", "ETAPA", "MUNICIPIO_HECHOS", "FISCAL_ASIGNADO", "DEPARTAMENTO"]
+# Actualizo el dataframe con las columnas de interés, ordenadas por fecha y reseteo el índice
+df= df[seleccion_columnas].sort_values(by="FECHA_HECHOS", ascending=True).reset_index(drop=True)
 
-#Convertir fechas
+#Convertir la columna FECHA_HECHOS a formato fecha
 df["FECHA_HECHOS"]= pd.to_datetime(df["FECHA_HECHOS"], errors="coerce")
 
 df_serie_tiempo = df.copy() 
@@ -27,16 +28,11 @@ st.dataframe (df)
 max_municipio = df["MUNICIPIO_HECHOS"].value_counts().index[0].upper()
 max_cantidad_municipio= df["MUNICIPIO_HECHOS"].value_counts().iloc[0]
 
-
-
-
-
-
 # Construir la página
 st.title("Dashboard de Delitos - Fiscalía")
 st.set_page_config(page_title="Dashboard de Delitos - Fiscalía", layout="centered") #or wide 
 st.header("Dashboard de Delitos - Fiscalía")
-st.dataframe(df)
+
 
 st.markdown(f"Municipio con más delitos: {max_municipio}", unsafe_allow_html=True)
 st.markdown(f"Cantidad de delitos: {max_cantidad_municipio}", unsafe_allow_html=True)
@@ -44,4 +40,8 @@ st.markdown(f"Cantidad de delitos: {max_cantidad_municipio}", unsafe_allow_html=
 st.subheader("Tipo de delito")
 delitos = df["DELITO"].value_counts()
 st.bar_chart(delitos)
+
+
+
+   
 
