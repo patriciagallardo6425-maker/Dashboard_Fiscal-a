c6d72f1f1a6a7577fb91f58081f9950529b20f7c
patriciagallardo6425-maker <patricia.gallardo6425@gmail.com>
2025-09-16 09:41:02 -0500

    hola

--- app.py
+++ app.py
@@ -1,48 +1,5 @@
 import pandas as pd
 import streamlit as st
-url = "https://github.com/patriciagallardo6425-maker/Misi-n-1-AI/raw/refs/heads/main/datos_generales_ficticios__.csv"
-df = pd.read_csv(url, sep=";", encoding="utf-8")
-
-
-st.dataframe(df)
-#Elegir columnas
-seleccion_columnas = ["FECHA_HECHOS", "DELITO", "ETAPA", "MUNICIPIO_HECHOS", "FISCAL_ASIGNADO", "DEPARTAMENTO"]
-# Actualizo el dataframe con las columnas de interés, ordenadas por fecha y reseteo el índice
-df= df[seleccion_columnas].sort_values(by="FECHA_HECHOS", ascending=True).reset_index(drop=True)
-
-#Convertir la columna FECHA_HECHOS a formato fecha
-df["FECHA_HECHOS"]= pd.to_datetime(df["FECHA_HECHOS"], errors="coerce")
-
-df_serie_tiempo = df.copy() 
-df_serie_tiempo["FECHA_HECHOS"]= df["FECHA_HECHOS"].dt.date
-
-filas= df.shape [0]
-columnas = df.shape[1]
-
-st.subheader("Datos ordenados por fecha de hechos")
-st.dataframe(df)
-st.write (f"Filas: {filas} \n Columnas: {columnas}")
-st.dataframe (df)
-
-#Cálculo de municipios con más delitos
-max_municipio = df["MUNICIPIO_HECHOS"].value_counts().index[0].upper()
-max_cantidad_municipio= df["MUNICIPIO_HECHOS"].value_counts().iloc[0]
-
-# Construir la página
-st.title("Dashboard de Delitos - Fiscalía")
-st.set_page_config(page_title="Dashboard de Delitos - Fiscalía", layout="centered") #or wide 
-st.header("Dashboard de Delitos - Fiscalía")
-
-
-st.markdown(f"Municipio con más delitos: {max_municipio}", unsafe_allow_html=True)
-st.markdown(f"Cantidad de delitos: {max_cantidad_municipio}", unsafe_allow_html=True)
-
-st.subheader("Tipo de delito")
-delitos = df["DELITO"].value_counts()
-st.bar_chart(delitos)
-
-import pandas as pd
-import streamlit as st
 import plotly.express as px
 
 url = 'https://github.com/patriciagallardo6425-maker/Misi-n-1-AI/raw/refs/heads/main/datos_generales_ficticios__.csv'
@@ -97,20 +54,6 @@ fig = px.bar(df_delitos, x='DEPARTAMENTO', y='conteo', color='DELITO', barmode='
 st.plotly_chart(fig)
 st.write(df_delitos)
 
-# Crear 4 columnas para las tarjetas
-col1, col2, col3, col4 = st.columns(4)
-
-with col1: 
-    # Tarjetas
-    ## Tarjeta 1 Municipio con más delitos
-    st.markdown (f"""# <h3 style="color: #F2A88D; background-color: #FFF6F5; border: 2px solid #F2A88d; border-radius: 10px; padding:10px;text-align: center"> Municipio con más delitos: <br>{max_municipio.upper()}</h3><br>""", unsafe_allow_html=True)
-
-with col2:
-
-    ## Tarjeta 2 Cantidad de delitos en el municipio con más delitos
-    st.markdown (f"""# <h3 style="color: #F2A88D; background-color: #FFF6F5; border: 2px solid #F2A88d; border-radius: 10px; padding:10px;text-align: center"> Delitos reportados <br> {max_cantidad_municipio} reportes</h3><br>""", unsafe_allow_html=True)
-
-
 st.subheader(f"Municipio con más delitos: {max_municipio} con {max_cantidad_municipio} reportes")
 st.subheader(f'{etapa_mas_frecuente} tiene {cant_etapa_mas_frecuente} registros')
 
@@ -122,13 +65,75 @@ st.subheader('Departamentos con mas Casos')
 departamento = df['DEPARTAMENTO'].value_counts()
 st.bar_chart(departamento)
 
-
-st.subheader("Distribución por Departamentos")
-fig = px.pie(
-	names=departamento.index,  # Para los nombres de la Torta
-	values=departamento.values # Para los valores de la Torta
-)
-
 fig.update_traces(textposition='outside', textinfo='percent+label')
 fig.update_layout(showlegend=False, height=400)
 st.plotly_chart(fig) 
+
+# Crear 4 columnas para las tarjetas
+col1, col2, col3, col4 = st.columns(4)
+
+with col1: 
+    # Tarjetas
+    ## Tarjeta 1 Municipio con más delitos
+    st.markdown (f"""# <h3 style="color: #F2A88D; background-color: #FFF6F5; border: 2px solid #F2A88d; border-radius: 10px; padding:10px;text-align: center"> Municipio con más delitos: <br>{max_municipio.upper()}</h3><br>""", unsafe_allow_html=True)
+
+with col2:
+
+    ## Tarjeta 2 Cantidad de delitos en el municipio con más delitos
+    st.markdown (f"""# <h3 style="color: #F2A88D; background-color: #FFF6F5; border: 2px solid #F2A88d; border-radius: 10px; padding:10px;text-align: center"> Delitos reportados <br> {max_cantidad_municipio} </h3><br>""", unsafe_allow_html=True)
+
+with col3:
+    ## Tarjeta 3 Etapa más recurrente
+    st.markdown (f"""# <h3 style="color: #FACF34; background-color: #FFF6F5; border: 2px solid #FACF34; border-radius: 10px; padding:10px;text-align: center"> Etapa más recurrente <br> {etapa_mas_frecuente} </h3><br>""", unsafe_allow_html=True)
+
+with col4:
+    ## Tarjeta 4 cantidad de casos con etapa más frecuente
+    st.markdown (f"""# <h3 style="color: #F2A88D; background-color: #FFF6F5; border: 2px solid #F2A88d; border-radius: 10px; padding:10px;text-align: center"> Procesos en esta etapa <br> {cant_etapa_mas_frecuente} registros </h3><br>""", unsafe_allow_html=True)
+
+
+# Crear columnas 5 y 6
+col5, col6 = st.columns(2)
+
+with col5:
+    st.header("Tipo de delitos")
+    tipo_delitos= df["DELITO"].value_counts()
+    st.bar_chart(tipo_delitos)
+
+with col6:
+    st.subheader("Distribución por Departamentos")
+    fig = px.pie(
+	names=departamento.index,  # Para los nombres de la Torta
+	values=departamento.values # Para los valores de la Torta
+    )
+    fig.update_traces(textposition="outside", textinfo="percent+label")
+    fig.update_layout(showlegend=False, height=350)
+    st.plotly_chart(fig, key="torta_departamentos")
+
+# Selección de dato para visualizar
+cols_grafico = ["DELITO", "ETAPA", "FISCAL_ASIGNADO", "DEPARTAMENTO", "MUNICIPIO_HECHOS"]
+df_grafico= df(cols_grafico)
+
+st.subheader ("Seleccione Dato a Visualizar")
+dato = st.selectbox(
+    "Seleccione la variable para el análisis:", 
+    options= df_grafico.columns
+)
+
+# st.subheader("Tipo delitos")
+
+grafico= df_grafico[dato].value_counts ()
+st.bar_chart (grafico)
+
+if st.checkbox ("Mostrar Matriz de Datos"):
+    st.subheader ("Matriz de Datos")
+    st.dataframe (df_grafico)
+
+# Consulta por fiscal asignado
+st.header ("Consulta por Fiscal Asignado")
+fiscal_consulta = st.selectbox(
+    "Selecciones el Fiscal a consultar:",
+    options = df["FISCAL_ASIGNADO"].unique()
+)
+
+df_fiscal = dt[df["FISCAL_ASIGNADO"]] ==fiscal_consulta
+st.dataframe (df_fiscal)
